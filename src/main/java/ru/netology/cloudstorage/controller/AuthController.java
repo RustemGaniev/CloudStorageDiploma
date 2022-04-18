9062c35a31bbcfb7789c23d32cf777a313e928d9
Vlad Semikin <vladsemikin3@gmail.com>
2022-04-18 19:01:49 +0200

    0.1

--- src/main/java/ru/netology/cloudstorage/controller/AuthController.java
+++ src/main/java/ru/netology/cloudstorage/controller/AuthController.java
@@ -1,14 +1,13 @@
 package ru.netology.cloudstorage.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.cloudstorage.exception_handling.UserAuthenticationException;
 import ru.netology.cloudstorage.dto.LoginDTO;
+import ru.netology.cloudstorage.exception_handling.UserAuthenticationException;
 import ru.netology.cloudstorage.security.JWTUtil;
 
 import java.util.Collections;
