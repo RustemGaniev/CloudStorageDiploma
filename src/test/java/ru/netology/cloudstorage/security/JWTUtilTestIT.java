f6385c3e797e034a1bf0a5ff591319aac4defe66
Vlad Semikin <vladsemikin3@gmail.com>
2022-05-08 09:01:55 +0200

    add tests

--- src/test/java/ru/netology/cloudstorage/security/JWTUtilTestIT.java
+++ src/test/java/ru/netology/cloudstorage/security/JWTUtilTestIT.java
@@ -7,24 +7,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import ru.netology.cloudstorage.CloudStorageApplication;
-import ru.netology.cloudstorage.repository.UserRepository;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -41,15 +34,15 @@ class JWTUtilTestIT {
     @Container
     static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>(DockerImageName.parse(
             "postgres:13.3"))
-            .withExposedPorts(5432)
+            .withExposedPorts(5432, 5432)
             .withDatabaseName("postgres")
             .withUsername("postgres")
             .withPassword("postgres");
 //            .withInitScript("classpath:db.sql");
 
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
     @Autowired
     private JWTUtil jwtUtil;
